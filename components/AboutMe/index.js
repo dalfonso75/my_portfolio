1dfc316e42a4301772b83efa2a83fc367537f76b
dalfonso75 <ea888416@gmail.com>
2022-06-20 12:50:49 -0500

    :recycle: refactory text

--- components/AboutMe/index.js
+++ components/AboutMe/index.js
@@ -1,32 +1,41 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import { motion } from "framer-motion";
 
 import SocialMedia from "../SocialMedia";
 import PrimaryButton from "../Button/PrimaryButton";
 
-import css from "./styles"
+import css from "./styles";
 
 export default function AboutMe() {
-    return (
-        <>
-            <motion.section className="c-about-me">
-                <section className="about-me__wrapper-media">
-                    <img className="about-me__image" src="./assets/perfile.webp" alt="Deiver Alfonso" />
-                    <SocialMedia />
-                    <img className="about-me__image-background" src="./assets/icons/rectangle.svg" />
-                </section>
-                <section className="about-me__wrapper-content">
-                    <h1 className="title">Deiver Enrique Alfonso Cortes</h1>
-                    <p className="paragraph">
-                        Soy un Fullstack Developer en JavaScript con 2 años de experiencia
-                        desarrollando API REST y sitios web. Poseo conocimientos en
-                        programación web y analítica de datos. Con experiencia en
-                        investigación e innovación. Cuento con gran capacidad de aprendizaje,
-                        trabajo individual, trabajo grupal y disciplina.
-                    </p>
-                    <PrimaryButton text={'VER CV'}/>
-                </section>
-            </motion.section>
-            <style jsx>{css}</style>
-        </>
-    );
+  return (
+    <>
+      <motion.section className="c-about-me">
+        <section className="about-me__wrapper-media">
+          <img
+            className="about-me__image"
+            src="./assets/perfile.webp"
+            alt="Deiver Alfonso"
+          />
+          <SocialMedia />
+          <img
+            className="about-me__image-background"
+            src="./assets/icons/rectangle.svg"
+          />
+        </section>
+        <section className="about-me__wrapper-content">
+          <h1 className="title">Deiver Enrique Alfonso Cortes</h1>
+          <p className="paragraph">
+            Soy Full Stack Developer en JavaScript con 2 años de experiencia en
+            Next.js, React js, Node js, Express, HTML, CSS y Python. Mi carrera
+            profesional es Ingenieria en Automatizacion, cuento con experiencia
+            en investigación e innovación y siempre dispuesto a aprender nuevas
+            tecnologías o herramientas.
+          </p>
+          <PrimaryButton text={"VER CV"} />
+        </section>
+      </motion.section>
+      <style jsx>{css}</style>
+    </>
+  );
 }
