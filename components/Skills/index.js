d7a8e3082ad4c4aa13014b5292bdac3937e898bf
dalfonso75 <ea888416@gmail.com>
2022-06-20 13:11:50 -0500

    add git in skills

--- components/Skills/index.js
+++ components/Skills/index.js
@@ -9,16 +9,17 @@ export default function Skills() {
       <SubtitleSection text={"Skills"} />
       <div className="c-icons-text--skills">
         <IconText srcImage={"./assets/icons/js.svg"} text={"JavaScript"} />
-        <IconText srcImage={"./assets/icons/nodejs.svg"} text={"Node JS"} />
-        <IconText srcImage={"./assets/icons/express.svg"} text={"Express"} />
         <IconText srcImage={"./assets/icons/next-js.svg"} text={"Next.js"} />
         <IconText srcImage={"./assets/icons/reactjs.svg"} text={"React JS"} />
+        <IconText srcImage={"./assets/icons/nodejs.svg"} text={"Node JS"} />
+        <IconText srcImage={"./assets/icons/express.svg"} text={"Express"} />
       </div>
       <div className="c-icons-text--skills">
-        <IconText srcImage={"./assets/icons/mysql.svg"} text={"MySQL"} />
         <IconText srcImage={"./assets/icons/html.svg"} text={"HTML"} />
         <IconText srcImage={"./assets/icons/css.svg"} text={"CSS"} />
+        <IconText srcImage={"./assets/icons/mysql.svg"} text={"MySQL"} />
         <IconText srcImage={"./assets/icons/python.svg"} text={"Python"} />
+        <IconText srcImage={"./assets/icons/git-icon.svg"} text={"Git"} />
       </div>
       <style jsx>{css}</style>
     </>
