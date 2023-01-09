e98c09e39ddb1dfdf7d17d288d1f792f52fc7d4c
dalfonso75 <ea888416@gmail.com>
2023-01-08 21:29:24 -0500

    [art] add Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
+import { Analytics } from "@vercel/analytics/react";
+
 import "normalize.css";
 import "locomotive-scroll/dist/locomotive-scroll.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
